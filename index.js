5409eeaa70e30fe6a1cc95af699847b4557f5e82
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-13 22:23:01 +0300

    commit tree f03632b67f771aa8399f01e3e269d963618c1828

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (lily) {
+  cats.push(lily);
+}
+
+function destructivelyPrependCat (lily) {
+  cats.unshift(lily);
+}
+
+function destructivelyRemoveLastCat () {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+  cats.shift();
+}
+
+function appendCat (lily) {
+  return [...cats, lily];
+}
+
+function prependCat (name) {
+  return [name, ...cats];
+}
+
+function removeFirstCat () {
+  return cats.slice(1);
+}
+
+function removeLastCat () {
+  return cats.slice(0, cats.length - 1);
+}
